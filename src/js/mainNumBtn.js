a17d6f9b51f152027f9988f82da54bcc239c8a05
O-Kreator <dongjunkang.design@gmail.com>
2020-12-06 22:46:30 +0900

    feat basic keyboard support

--- src/js/mainNumBtn.js
+++ src/js/mainNumBtn.js
@@ -1,11 +1,18 @@
-import { DOM } from './_config.js';
+import { CONST, DOM } from './_config.js';
 import textFunc from './text';
 import { btnHistoryFunc } from './status';
+import helperFunc from './helper.js';
 
 const mainNumBtnFunc = {
   event(text) {
     textFunc.main.input(text);
     textFunc.main.update();
+  },
+  keyDown(text, btn) {
+    mainNumBtnFunc.event(text);
+
+    btn.classList.add("pressed");
+    setTimeout(() => { btn.classList.remove("pressed") }, CONST.TIME_SHORT);
   },
 
   init() {
@@ -15,11 +22,23 @@ const mainNumBtnFunc = {
         btnHistoryFunc.update(e.target);
       });
     }
+    document.addEventListener("keydown", e => {
+      if (helperFunc.isNumber(e.key) && !DOM.mainBtn[`num${e.key}`].disabled) {
+        mainNumBtnFunc.keyDown(e.key, DOM.mainBtn[`num${e.key}`]);
+        btnHistoryFunc.update(DOM.mainBtn[`num${e.key}`]);
+      }
+    });
 
     DOM.mainBtn.dot.addEventListener("click", () => {
       mainNumBtnFunc.event(".");
       btnHistoryFunc.update(DOM.mainBtn.dot);
     });
+    document.addEventListener("keydown", e => {
+      if (e.key === "." && !DOM.mainBtn.dot.disabled) {
+        mainNumBtnFunc.keyDown(e.key, DOM.mainBtn.dot);
+        btnHistoryFunc.update(DOM.mainBtn.dot);
+      }
+    })
   }
 }
 
--- src/js/mainOperatorBtn.js
+++ src/js/mainOperatorBtn.js
@@ -1,6 +1,7 @@
-import { DOM } from './_config.js';
+import { CONST, DOM } from './_config.js';
 import textFunc from './text';
 import { btnHistoryFunc } from './status';
+import helperFunc from './helper.js';
 
 const mainOperatorBtnFunc = {
   operator(text) {
@@ -8,9 +9,21 @@ const mainOperatorBtnFunc = {
     textFunc.sub.preCalculate();
     textFunc.sub.update();
   },
+  operatorKeydown(text, btn) {
+    mainOperatorBtnFunc.operator(text);
+
+    btn.classList.add("pressed");
+    setTimeout(() => { btn.classList.remove("pressed") }, CONST.TIME_SHORT);
+  },
   equal() {
     textFunc.sub.calculate();
     textFunc.sub.update();
+  },
+  equalKeydown(btn) {
+    mainOperatorBtnFunc.equal();
+
+    btn.classList.add("pressed");
+    setTimeout(() => { btn.classList.remove("pressed") }, CONST.TIME_SHORT);
   },
 
   init() {
@@ -18,22 +31,56 @@ const mainOperatorBtnFunc = {
       mainOperatorBtnFunc.operator("+");
       btnHistoryFunc.update(DOM.mainBtn.plus);
     });
+    document.addEventListener("keydown", e => {
+      if (e.key === "+" && !DOM.mainBtn.plus.disabled) {
+        mainOperatorBtnFunc.operatorKeydown(e.key, DOM.mainBtn.plus);
+        btnHistoryFunc.update(DOM.mainBtn.plus);
+      }
+    });
+
     DOM.mainBtn.minus.addEventListener("click", () => {
       mainOperatorBtnFunc.operator("-");
       btnHistoryFunc.update(DOM.mainBtn.minus);
     });
+    document.addEventListener("keydown", e => {
+      if (e.key === "-" && !DOM.mainBtn.minus.disabled) {
+        mainOperatorBtnFunc.operatorKeydown(e.key, DOM.mainBtn.minus);
+        btnHistoryFunc.update(DOM.mainBtn.minus);
+      }
+    });
+
     DOM.mainBtn.multiply.addEventListener("click", () => {
       mainOperatorBtnFunc.operator("*");
       btnHistoryFunc.update(DOM.mainBtn.multiply);
     });
+    document.addEventListener("keydown", e => {
+      if (e.key === "*" && !DOM.mainBtn.multiply.disabled) {
+        mainOperatorBtnFunc.operatorKeydown(e.key, DOM.mainBtn.multiply);
+        btnHistoryFunc.update(DOM.mainBtn.multiply);
+      }
+    });
+
     DOM.mainBtn.divide.addEventListener("click", () => {
       mainOperatorBtnFunc.operator("/");
       btnHistoryFunc.update(DOM.mainBtn.divide);
     });
+    document.addEventListener("keydown", e => {
+      if (e.key === "/" && !DOM.mainBtn.divide.disabled) {
+        mainOperatorBtnFunc.operatorKeydown(e.key, DOM.mainBtn.divide);
+        btnHistoryFunc.update(DOM.mainBtn.divide);
+      }
+    });
+
     DOM.mainBtn.equal.addEventListener("click", () => {
       mainOperatorBtnFunc.equal();
       btnHistoryFunc.update(DOM.mainBtn.equal);
     })
+    document.addEventListener("keydown", e => {
+      if ((e.key === "=" || e.key === "Enter") && !DOM.mainBtn.equal.disabled) {
+        mainOperatorBtnFunc.equalKeydown(DOM.mainBtn.equal);
+        btnHistoryFunc.update(DOM.mainBtn.equal);
+      }
+    });
   }
 }
 
